777525c184a9fdebc4d99f18100f7cca288c3f2a
Hackpoetic <chao-mu@hackpoetic.com>
2019-03-17 01:56:23 -0400

    Finalize art 00001

--- 00001/README.md
+++ 00001/README.md
@@ -1,1 +1,9 @@
-Source of input image: https://www.publicdomainpictures.net/en/view-image.php?image=1989&picture=angel-statue
+# 00001
+
+I am recursively modifying an image with a fragment shader.
+
+The heart of the code is in the fragment shader file `frag.glsl`, the rest is OpenGL magic and conveniences for me like shader hot-swapping.
+
+## Sources
+angel-statue.jpg - https://www.publicdomainpictures.net/en/view-image.php?image=1989&picture=angel-statue
+skull-and-crossbones.jpg - https://www.publicdomainpictures.net/en/view-image.php?image=130392&picture=skull-and-crossbones
--- 00001/frag.glsl
+++ 00001/frag.glsl
@@ -2,29 +2,73 @@
 
 layout (location = 0) out vec4 FragColor;
 
-uniform float iTime;
+// width and height of image
 uniform vec2 iResolution;
 
+// The image texture
 uniform sampler2D img0;
+
+// The last output of this shader
 uniform sampler2D lastOut;
+
+// Whether or not this is the first time this shader is being run
 uniform bool firstPass;
 
 in vec2 texcoord;
 
-#define MAX_STEPS 100
+// Random number in the range 0 >= x < 1
+float rand(vec2 v) {
+    return fract(sin(dot(v, vec2(23.405, 45.324))) * 3405.563);
+}
 
-mat2 rotate(float a) {
-    return mat2(cos(a), -sin(a), sin(a), cos(a));
+// Based on code from The Book of Shaders
+// Perlin noise.
+float noise(vec2 v) {
+    vec2 i = floor(v);
+    vec2 f = fract(v);
+
+    // Four corners in 2D of a tile
+    float a = rand(i);
+    float b = rand(i + vec2(1.0, 0.0));
+    float c = rand(i + vec2(0.0, 1.0));
+    float d = rand(i + vec2(1.0, 1.0));
+
+    vec2 u = smoothstep(0., 1., f);
+
+    // Mix 4 corners percentages
+    return mix(a, b, u.x) +
+            (c - a)* u.y * (1.0 - u.x) +
+            (d - b) * u.x * u.y;
+}
+
+// The vector field we use to modify the image;
+vec2 vfield() {
+    vec2 uv = (gl_FragCoord.xy - .5 * iResolution) / iResolution.y;
+
+    float x = noise(uv * 10);
+
+    return vec2(cos(x), sin(x));
 }
 
 void main() {
     vec2 texcoord = gl_FragCoord.xy / iResolution;
     if (firstPass) {
         FragColor = texture(img0, texcoord);
-    } else {
-        vec3 col = texture(lastOut, texcoord).rgb;
-        FragColor.rgb = col;
+        return;
     }
 
-    FragColor.a = 1;
+    vec3 col = texture(lastOut, texcoord).rgb;
+    vec2 uv = (texcoord - .5) * 2.;
+
+    vec2 o = vfield();
+    vec2 rUv = mix(uv, uv * o, 0.001);
+    vec2 gUv = mix(uv, uv * o, 0.002);
+    vec2 bUv = mix(uv, uv * o, 0.003);
+
+    FragColor = vec4(
+        texture(lastOut, rUv * .5 + .5).r,
+        texture(lastOut, gUv * .5 + .5).g,
+        texture(lastOut, bUv * .5 + .5).b,
+        1
+    );
 }
